4760e0694e0e9ce7d897ad1f88b3d609268eebd1
Daniele Bonadiman <bonadiman.daniele@gmail.com>
2014-04-14 16:28:00 +0200

    no enter

--- KillahWSAD.c
+++ KillahWSAD.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <termios.h>
 #include <time.h>
 #define N	10
 
@@ -9,8 +10,30 @@ short int arena(int player[], int enemy[], short int death);
 int move (char arrow, int player[], int enemy[]);
 void refresh();
 
+
+char getch() {
+        char buf = 0;
+        struct termios old = {0};
+        if (tcgetattr(0, &old) < 0)
+                perror("tcsetattr()");
+        old.c_lflag &= ~ICANON;
+        old.c_lflag &= ~ECHO;
+        old.c_cc[VMIN] = 1;
+        old.c_cc[VTIME] = 0;
+        if (tcsetattr(0, TCSANOW, &old) < 0)
+                perror("tcsetattr ICANON");
+        if (read(0, &buf, 1) < 0)
+                perror ("read()");
+        old.c_lflag |= ICANON;
+        old.c_lflag |= ECHO;
+        if (tcsetattr(0, TCSADRAIN, &old) < 0)
+                perror ("tcsetattr ~ICANON");
+        return (buf);
+}
+
+
 int main ()
-{	
+{
 	short int death=0;
 	int player[2], enemy[2], i;
 	char arrow, any;
@@ -25,9 +48,9 @@ int main ()
 	while (death!=1)
 	{
 		//scanf("%c", &arrow);
-		arrow= getchar();
-		move (arrow, player, enemy);	
-		refresh();	
+		arrow= getch();
+		move (arrow, player, enemy);
+		refresh();
 		arena(player, enemy, death);
 	}
 	/*while (death!=1)
@@ -46,23 +69,23 @@ int arena_init(int player[2], int enemy[2])
 	player[1]=4;
 	enemy[0]=9;
 	enemy[1]=9;
-	
-	
+
+
 	printf("\n");
 	for (i=0; i<N; i++)
 	{
 		for (j=0; j<N; j++)
-		{	
+		{
 			if (player[0]==i && player[1]==j)
 			{ 	ARENA[i][j] = 'W';	}
 			else
 			if (enemy[0]==i && enemy[1]==j)
-			{	ARENA[i][j] = 'X';		} 
+			{	ARENA[i][j] = 'X';		}
 			else
 			{	ARENA[i][j] = 183;	}
-		}	
-	}	
-	
+		}
+	}
+
 	for (i=0; i<N; i++)
 	{
 		for (j=0; j<N; j++)
@@ -70,10 +93,10 @@ int arena_init(int player[2], int enemy[2])
 			printf(" %c ", ARENA[i][j]);
 		}
 		printf("\n");
-	
-	}	
+
+	}
 	printf("\n");
-	
+
 	return player[2];
 	return enemy[2];
 
@@ -83,7 +106,7 @@ void refresh ()
 {
 	int i;
 		for (i=0; i<(N+2); i++)
-		{	
+		{
 			   fputs("\033[A\033[2K",stdout);
 		}
 		//fputs("\033[A\033[2K",stdout);
@@ -94,10 +117,10 @@ short int arena (int player[2], int enemy[2], short int death)
 {
 	int i, j;
 	char ARENA[N][N];
-	   
+
 	printf("\n");
 	if (player[0]==enemy[0] && player[1]==enemy[1])
-	{	
+	{
 		printf("\n\n\nYOU ATE THE MOTHEFUCKER!!!!\n\n\n");
 		death= 1;
 	}
@@ -105,17 +128,17 @@ short int arena (int player[2], int enemy[2], short int death)
 	{		for (i=0; i<N; i++)
 			{
 				for (j=0; j<N; j++)
-				{	
+				{
 					if (player[0]==i && player[1]==j)
 					{ 	ARENA[i][j] = 'W';	}
 					else
 					if (enemy[0]==i && enemy[1]==j)
-					{	ARENA[i][j] = 'X';		} 
+					{	ARENA[i][j] = 'X';		}
 					else
 					{	ARENA[i][j] = 183;	}
-				}	
-			}	
-	
+				}
+			}
+
 			for (i=0; i<N; i++)
 			{
 				for (j=0; j<N; j++)
@@ -123,57 +146,57 @@ short int arena (int player[2], int enemy[2], short int death)
 					printf(" %c ", ARENA[i][j]);
 				}
 				printf("\n");
-	
-			}	
+
+			}
 			//printf("\n");
 	}
-	
+
 	return death;
 }
 
 
 int move (char arrow, int player[2], int enemy[2])
-{	
+{
 
-	
-	
+
+
 			if(arrow== 'w')
-			{ 
+			{
 				if ((player[0]-1)<0)
 				{player[0]= N+(player[0]-1);}
 				else
-				{player[0]= player[0]-1;}	
+				{player[0]= player[0]-1;}
 			}
 			if(arrow== 's')
 			{ player[0]=(player[0]+1)%N;}
 			if(arrow== 'd')
 			{ player[1]=(player[1]+1)%N;}
 			if(arrow== 'a')
-			{ 
+			{
 				if ((player[1]-1)<0)
 				{player[1]= N+(player[1]-1);}
 				else
-				{player[1]= player[1]-1;}	
+				{player[1]= player[1]-1;}
 			}
-	
-	
-	
+
+
+
 		//	printf("%d  %d", player[0], player[1]);
-			
+
 					srand(time(NULL));
-					float x,y;	
+					float x,y;
 					x=(float) rand()/(RAND_MAX);
 					y=(float) rand()/(RAND_MAX);
-	
+
 					if(y > 0.5)
 					{
 						if(x<=0.5)
-						{ 
+						{
 							if ((enemy[0]-1)<0)
 							{enemy[0]= N+(enemy[0]-1);}
 							else
-							{enemy[0]= enemy[0]-1;}	
-		
+							{enemy[0]= enemy[0]-1;}
+
 						}
 						else
 						{ enemy[0]=(enemy[0]+1)%N;}
@@ -181,26 +204,26 @@ int move (char arrow, int player[2], int enemy[2])
 					else
 					{
 						if(x<=0.5)
-						{ 
-							
+						{
+
 							if ((enemy[1]-1)<0)
 							{enemy[1]= N+(enemy[1]-1);}
 							else
-							{enemy[1]= enemy[1]-1;}	
-		
-							
+							{enemy[1]= enemy[1]-1;}
+
+
 						}
 						else
 						{ enemy[1]=(enemy[1]+1)%N;}
 					}
-		
-	
+
+
 	arrow='o';
 	return arrow;
 	return player[2];
 	return enemy[2];
-	
 
-	
-	
+
+
+
 }
